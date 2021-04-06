2023d56aaa8972f9b93233f66770e4f251f622c7
Blaiteray <66131047+Blaiteray@users.noreply.github.com>
2021-04-06 21:11:05 +0600

    adding textbox

--- main.cpp
+++ main.cpp
@@ -3,8 +3,21 @@
 int main() {
   sf::RenderWindow window(sf::VideoMode(820,540),"Blaiteray Antivirus",sf::Style::Titlebar|sf::Style::Close);
   sf::String getPinp="";
-  Text hW("", sf::Vector2f(6,6));
+  sdx::Text hW("", sf::Vector2f(6,6));
   sf::RectangleShape rect;
+
+  sf::RectangleShape outerRect;
+  sf::RectangleShape innerRect;
+  outerRect.setSize(sf::Vector2f(460,36));
+  innerRect.setSize(sf::Vector2f(456,32));
+  outerRect.setPosition(sf::Vector2f(100,100));
+  innerRect.setPosition(sf::Vector2f(102,102));
+  outerRect.setFillColor(sf::Color::Black);
+  innerRect.setFillColor(sf::Color::White);
+
+
+
+
   rect.setSize(sf::Vector2f(820,1));
   rect.setPosition(sf::Vector2f(0,28));
   rect.setFillColor(sf::Color::Black);
@@ -27,6 +40,8 @@ int main() {
     window.clear(sf::Color::White);
     window.draw(hW.get());
     window.draw(rect);
+    window.draw(outerRect);
+    window.draw(innerRect);
     window.display();
   }
 
--- src/TextBox.hpp
+++ src/TextBox.hpp
@@ -1,11 +1,21 @@
 #include<SFML/Graphics.hpp>
 
-class Text {
-private:
-  sf::Font font;
-  sf::Text text;
-public:
-  Text(sf::String, sf::Vector2f);
-  sf::Text get();
-  void set(sf::String);
-};
+namespace sdx {
+  class Text {
+  private:
+    sf::Font font;
+    sf::Text text;
+  public:
+    Text(sf::String, sf::Vector2f);
+    sf::Text get();
+    void set(sf::String);
+  };
+
+  class TextBox {
+  private:
+    Text inpText;
+  public:
+    TextBox();
+    void draw(sf::RenderWindow &);
+  };
+}
